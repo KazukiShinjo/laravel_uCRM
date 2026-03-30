a75455e01669aba24def693aedd781b9517bfdb7
KazukiShinjo <kazuki.03225@Outlook.com>
2026-03-30 16:30:56 +0900

    customers

---
+++ resources/js/Pages/Customers/Index copy.vue
@@ -0,0 +1,20 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, Link } from '@inertiajs/vue3';
+import FlashMessage from '@/Components/FlashMessage.vue';
+import { onMounted } from 'vue';
+
+const props = defineProps({
+  customers: Object
+})
+
+// ページが表示されたタイミングで実行
+onMounted(() => {
+  console.log(props.customers);
+  console.log(props.customers.last_page);
+});
+</script>
+
+<template>
+    
+</template>
